403454059a505f84449494b702e225c45a45a7a3
Beli <belinda-pro@outlook.com>
2025-10-28 22:00:29 +0000

    ploulp

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 600,
-    height: 600,
+    width: 1000,
+    height: 750,
     frame: false,
-    transparent: false,
+    transparent: true,
     webPreferences: {
       sandbox: false,
       nodeIntegration: true,
@@ -19,7 +19,7 @@ function createWindow() {
   mainWindow.webContents.setDevToolsWebContents(devtools.webContents);
   mainWindow.webContents.openDevTools({ mode: 'detach' });
 }
-/*
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -41,6 +41,5 @@ ipcMain.on('close-window', () => {
   const window = BrowserWindow.getFocusedWindow();
   if (window) window.close();
 });
-*/
 
 app.whenReady().then(createWindow);
